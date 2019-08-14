5640cb3053d14d54cf4c8ae0bacc61517cd55879
JJ <jonjolivette777@aol.com>
2019-08-14 10:30:46 -0500

    initial commit | added primary structure/markup & style elements

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// ==========================================================
+// Project: Public API Request
+// File: scripts.js (Main JavaScript Logic)
+// Author: Jonathan J.Jolivette
+// Created: 8.14.19
+// THANK YOU FOR YOUR SUPPORT
+// ==========================================================
+
+/* DOMContentLoaded...*/ window.addEventListener('DOMContentLoaded', (event) => {}); // <---- conclusion of DOMContentLoaded function
