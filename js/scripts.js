be506d25ee3d19512c3a30fdb980aa70f05ff34e
JJ <jonjolivette777@aol.com>
2019-08-14 17:13:29 -0500

    added fetch request to API, response data set to displays 1 random employee's info on card

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,53 @@
 // THANK YOU FOR YOUR SUPPORT
 // ==========================================================
 
-/* DOMContentLoaded...*/ window.addEventListener('DOMContentLoaded', (event) => {}); // <---- conclusion of DOMContentLoaded function
+/* DOMContentLoaded...*/ window.addEventListener('DOMContentLoaded', (event) => {
+	// ================
+	// GLOBAL VARIABLES
+	// ================
+	// let name = document.getElementById('name');
+
+	// ================
+	// FETCH FUNCTIONS
+	// ================
+	fetch('https://randomuser.me/api/?results=12').then((res) => res.json()).then((data) => generateName(data));
+
+	fetch('https://randomuser.me/api/?results=12').then((res) => res.json()).then((data) => generateEmail(data));
+
+	fetch('https://randomuser.me/api/?results=12').then((res) => res.json()).then((data) => generateLocation(data));
+
+	fetch('https://randomuser.me/api/?results=12').then((res) => res.json()).then((data) => generateImage(data));
+
+	// ================
+	// HELPER FUNCTIONS
+	// ================
+	function generateName(data) {
+		let name = document.getElementById('name');
+		const firstName = data.results[0].name.first;
+		const lastName = data.results[0].name.last;
+		const fullName = `${firstName} ${lastName}`;
+		name.innerText = fullName;
+	}
+
+	function generateEmail(data) {
+		let email = document.querySelector('p.card-text');
+		const emailAddress = data.results[0].email;
+		const userEmail = `${emailAddress}`;
+		email.innerText = userEmail;
+	}
+
+	function generateLocation(data) {
+		let address = document.querySelector('p.card-text.cap');
+		const cityAddress = data.results[0].location.city;
+		const stateAddress = data.results[0].location.state;
+		const location = `${cityAddress} ${stateAddress}`;
+		console.log((address.innerText = location));
+		address.innerText = location;
+	}
+
+	function generateImage(data) {
+		let imgFrame = document.querySelector('#gallery img');
+		const avatar = data.results[0].picture.thumbnail;
+		imgFrame.src = avatar;
+	}
+}); // <---- conclusion of DOMContentLoaded function
