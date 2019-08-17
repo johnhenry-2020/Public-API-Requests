da107d85053cba6e099b53f678f3228369f6cbf5
JJ <jonjolivette777@aol.com>
2019-08-17 09:00:36 -0500

    added functionality for data to populate to DOM

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,9 @@
 // ==========================================================
 
 /* DOMContentLoaded...*/ window.addEventListener('DOMContentLoaded', (event) => {
-	// ================
-	// GLOBAL VARIABLES
-	// ================
-	// let name = document.getElementById('name');
-
-	// ================
+	// ========================================
 	// FETCH FUNCTIONS - using HTML5 fetch API
-	// ================
+	// ========================================
 	function fetchData(url) {
 		// No 'Access-Control-Allow-Origin' addresed by setting the mode of fetch to CORS
 		// REFERENCE: https://stackoverflow.com/questions/36878255/allow-access-control-allow-origin-header-using-html5-fetch-api
@@ -26,30 +21,42 @@
 
 	fetchData('https://randomuser.me/api/?results=12').then((data) => {
 		// =====  PROMISE ITERATES THROUGH THE JSON DATA RETURNED & CREATES THE EMPLOYEE'S PROFILE CARDS DYNAMICALLY =====
-		console.log(data);
-		const employee = data.results.map((data) => {
+		const employee = data.results.map((data, name) => {
+			// ==== CARD DIV ====
 			var cardDiv = document.createElement('div'); // Create a <div> element for card div
 			cardDiv.setAttribute('class', 'card zoom'); // Add class(es) to card <div> element
+
+			// ==== CARD IMAGE CONTAINER DIV ====
 			var cardImgContainer = document.createElement('div'); // Create a <div> element for card image container
 			cardImgContainer.setAttribute('class', 'card-img-container'); //Add class(es) to card image container <div>
+
+			// ==== AVATAR PLACEHOLDER ====
 			var cardImg = new Image(); // Create an image element for profile pictures
 			cardImg.setAttribute('class', 'card-img avatar'); //Add class(es) to card <div> element
-			cardImg.src = 'https://placehold.it/90x90'; //setup the image's source/path
+			cardImg.src = data.picture.thumbnail; // populates the API's JSON employee picture/thumbnail property value to the DOM
 			cardImg.alt = 'profile picture'; //set the alt attribute
+
+			// ==== CARD INFO CONTAINER DIV ====
 			var cardInfoContainer = document.createElement('div'); // Create a card info container <div> element for profile's text elements
 			cardInfoContainer.setAttribute('class', 'card-info-container'); // Add class(es) to card info container <div> element
-			var cardProfileName = document.createElement('h3'); // Create a <h3> element for profile card name
-			cardProfileName.setAttribute('id', 'name');
-			cardProfileName.setAttribute('class', 'card-name cap');
-			cardProfileName.innerText = 'First Last';
-			var cardProfileEmail = document.createElement('P'); // Create a <p> element for profile card email
-			cardProfileEmail.setAttribute('class', 'card-text email');
-			cardProfileEmail.innerText = 'Email';
-			var cardProfileLocation = document.createElement('P'); // Create a <p> element for profile card location/address
-			cardProfileLocation.setAttribute('class', 'card-text cap location');
-			cardProfileLocation.innerText = 'City State';
 
-			// ======  flow/structure of all dynamically created card elements...  ======
+			// ==== H3 TEXT ELEMENT FOR NAME ====
+			var cardProfileName = document.createElement('h3'); // Create a <h3> element for profile card name
+			cardProfileName.setAttribute('id', 'name'); // Add id attribute to header element
+			cardProfileName.setAttribute('class', 'card-name cap'); // Add class(es) to card header h3 text element
+			cardProfileName.innerText = data.name.first + ' ' + data.name.last; // populates the API's JSON first and last name property values to the DOM
+
+			// ==== PARAGRAPH TEXT ELEMENT FOR EMAIL ====
+			var cardProfileEmail = document.createElement('P'); // Create a <p> element for profile card's email info
+			cardProfileEmail.setAttribute('class', 'card-text email'); // Add class(es) to card's paragraph text element for emails
+			cardProfileEmail.innerText = data.email; // populates the API's JSON email property value to the DOM
+
+			// ==== PARAGRAPH TEXT ELEMENT FOR LOCATION (CITY, STATE) ====
+			var cardProfileLocation = document.createElement('P'); // Create a <p> element for profile card location/address
+			cardProfileLocation.setAttribute('class', 'card-text cap location'); // Add class(es) to card's paragraph text element for locations
+			cardProfileLocation.innerText = data.location.city + '' + data.location.state; // populates the API's JSON location (city/state) property value to the DOM
+
+			// ======  the block is responsible for the flow/structure of all dynamically created card elements...  ======
 			document.body.appendChild(cardDiv);
 			cardDiv.appendChild(cardImgContainer);
 			cardImgContainer.appendChild(cardImg);
@@ -59,23 +66,13 @@
 			cardProfileEmail.appendChild(cardProfileLocation);
 			const galleryDiv = document.getElementById('gallery');
 			galleryDiv.appendChild(cardDiv);
-			console.log(cardDiv);
-
-			// generateName(data);
+			// ============================================================================================================
 		}); //conclusion of employee map method
-		// const name = data.results.map((data) => generateName(data));
-		// const email = data.results.map((data) => generateEmail(data));
-		// const address = data.results.map((data) => generateLocation(data));
-		// const image = data.results.map((data) => generateImage(data));
-	});
-	// .then(data.results.map((data) => generateName(data)))
-	// .then(data.results.map((data) => generateEmail(data)))
-	// .then(data.results.map((data) => generateLocation(data)))
-	// .then(data.results.map((data) => generateImage(data)));
+	}); //conclusion of fetchData method
 
-	// ================
+	// =================
 	// HELPER FUNCTIONS
-	// ================
+	// =================
 	function generateName(data) {
 		let name = document.getElementById('name');
 		const firstName = data.name.first;
@@ -107,9 +104,9 @@
 		imgFrame.src = avatar;
 	}
 
-	// ================
+	// =========================
 	// ERROR HANDLING FUNCTIONS
-	// ================
+	// =========================
 	function checkstatus(response) {
 		if (response.ok === true) {
 			return Promise.resolve(response);
